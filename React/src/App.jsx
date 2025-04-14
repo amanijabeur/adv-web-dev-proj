8ffd494c55cc445edfab216b4a899d1a7315dc98
amanijabeur <amenijabeur29@gmail.com>
2025-04-15 00:59:53 +0100

    Update line endings and project files

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './style.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Hello React</h1>
+      <header>
+        <h1>Welcome to My Website</h1>
+        <p>This is my first website using React!</p>
+      </header>
 
-export default App
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>Hello, I am Ameni Jabeur, a sophomore student at TBS.</p>
+        </section>
+      </main>
+
+      <footer>
+        <p>© 2025 React Project Website by Ameni Jabeur</p>
+      </footer>
+    </div>
+  );
+}
+export default App;
+
+
+
