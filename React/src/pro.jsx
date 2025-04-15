ebc107560c4753496acc3f79b9620e1997fdca9d
amanijabeur <amenijabeur29@gmail.com>
2025-04-15 04:45:01 +0100

    W4 React Multiple Components

--- React/src/pro.jsx
+++ React/src/pro.jsx
@@ -22,30 +22,41 @@ const list = [
 function getTitle(title) {
     return title;
 }
+function List(){
+    return(
+        <ul>
+            {list.map(function (item) {
+                return(
+                    <li key={item.object}>
+                    <span>
+                    <a href={item.url}>{item.title}</a> 
+                    </span> 
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                    </li>
+                );
+            })}
+        </ul>
+    )
+}
+function Search() {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+        );
+    }
 function Pro() {
 
   return (
     <div>
       <h1>Hello {getTitle("React")}</h1>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label> 
-      <input id="search" type="text" />
+      <Search />
       <hr />
-      <ul>
-            {list.map(function (item) {
-            return(
-                 <li key={item.object}>
-                 <span>
-                    <a href={item.url}>{item.title}</a> 
-                 </span> 
-                 <span>{item.author}</span>
-                 <span>{item.num_comments}</span>
-                 <span>{item.points}</span>
-                </li>
-            );
-            })}
-      </ul>
-
+      <List />
     </div>
   );
 }
